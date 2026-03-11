5c5592028a331c6630c60c0b43d07d214ad0278d
thkernel <salut.amos@gmail.com>
2026-03-11 14:06:04 +0000

    update

--- app/controllers/concerns/internal_auth.rb
+++ app/controllers/concerns/internal_auth.rb
@@ -10,6 +10,7 @@ module InternalAuth
     def authenticate_service!
         token = request.headers["Authorization"]&.split(" ")&.last
         Rails.logger.info("[InternalAuth] authenticate_service! called, token present: #{token.present?}")
+            Rails.logger.info("[InternalAuth] authenticate_service! called | token reçu: #{token.inspect} | token attendu: #{ENV['INTERNAL_SERVICE_TOKEN'].inspect}")
 
         unless token == ENV["INTERNAL_SERVICE_TOKEN"]
             Rails.logger.warn("[InternalAuth] Unauthorized service attempt")
