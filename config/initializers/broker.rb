88d9599ac560ce90b96a9889c97801a37b272e7a
thkernel <salut.amos@gmail.com>
2026-03-26 13:14:02 +0000

    update

--- config/initializers/broker.rb
+++ config/initializers/broker.rb
@@ -5,11 +5,11 @@ BROKER_TYPE = ENV['BROKER_TYPE'] || 'rabbitmq'
 
 case BROKER_TYPE
 when 'rabbitmq'
-  BROKER_CONN = Bunny.new(ENV['RABBITMQ_URL'] || 'amqp://admin:Root2026@localhost:5672')
+  BROKER_CONN = Bunny.new(ENV['RABBITMQ_URL'] || 'amqp://admin:Root2026@127.0.0.1:5672')
   BROKER_CONN.start
   BROKER_CHANNEL = BROKER_CONN.create_channel
 when 'kafka'
-  BROKER_CONN = Kafka.new(seed_brokers: [ENV['KAFKA_BROKER'] || 'localhost:9092'], client_id: 'otp_service')
+  BROKER_CONN = Kafka.new(seed_brokers: [ENV['KAFKA_BROKER'] || '127.0.0.1:9092'], client_id: 'otp_service')
 else
   raise "BROKER_TYPE inconnu: #{BROKER_TYPE}"
 end
